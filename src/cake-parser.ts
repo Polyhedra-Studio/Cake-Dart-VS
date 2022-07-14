477511ec4067ecde65f7f58eaed66fcc36d4bf27
CLeeSpruit <c.lee.spruit@polyhedra.studio>
2022-07-14 14:52:19 -0500

    Cleaned up a LOT of code

--- src/cake-parser.ts
+++ src/cake-parser.ts
@@ -1,40 +1,100 @@
 import * as vscode from 'vscode';
 
-const testRunnerRe = /TestRunner(WithContext)?(<.*>)?\(('.*'|".*")/;
-const groupRe = /Group(WithContext)?(<.*>)?\(('.*'|".*")/;
-const testRe = /Test(WithContext)?(<.*>)?\(('.*'|".*")/;
+const testRunnerReShort = /TestRunner(WithContext)?\b/;
+const groupReShort = /Group(WithContext)?\b/;
+const testReShort = /Test(WithContext)?\b/;
+const hasNameRe = /(["'])((?:\\1|(?:(?!\1)).)*)(\1|$)/;
 
 export const parseCakeTest = (text: string, events: {
     onTest(range: vscode.Range, testName: string): void;
     onTestRunner(range: vscode.Range, testRunnerName: string): void;
-    onGroup(range: vscode.Range, groupName: string, depth: number): void;
+    onGroup(range: vscode.Range, groupName: string): void;
+	onAscend(): void;
 }) => {
     const lines = text.split('\n');
+	let foundItemEvent: ((range: vscode.Range, name: string) => void) | null = null;
 
+	const matchName = (line: string, rangeIndex: number): boolean => {
+		if (foundItemEvent) {
+			const hasName = hasNameRe.exec(line);
+			if (hasName) {
+				const [match, , name] = hasName;
+				const range = new vscode.Range(new vscode.Position(rangeIndex, 0), new vscode.Position(rangeIndex, match.length));
+				foundItemEvent(range, name);
+				foundItemEvent = null;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Returns the matching result for digging out the where the opening bracket to watch should be
+	const testForItem = (shortRe: RegExp, event: (range: vscode.Range, name: string) => void, line: string, index: number): string => {
+		const test = shortRe.exec(line);
+		if (test) {
+			const hasName = hasNameRe.exec(line);
+			if (hasName) {
+				const [match, , name] = hasName;
+				const range = new vscode.Range(new vscode.Position(index, 0), new vscode.Position(index, match.length));
+				event(range, name);
+			} else {
+				foundItemEvent = event;
+			}
+			const [matchResult] = test;
+			return matchResult;
+		}
+		return '';
+	}
+
+	const bracketWatch = [];
+	let bracketIndex = 0;
 	for (let lineNo = 0; lineNo < lines.length; lineNo++) {
 		const line = lines[lineNo];
-		const test = testRe.exec(line);
-		if (test) {
-			const [match, , , testName] = test;
-			const range = new vscode.Range(new vscode.Position(lineNo, 0), new vscode.Position(lineNo, match.length));
-			events.onTest(range, testName);
-			continue;
+		let matchWatch: string = '';
+
+		matchWatch = testForItem(testRunnerReShort, events.onTestRunner, line, lineNo);
+		if (!matchWatch) {
+			matchWatch = testForItem(groupReShort, events.onGroup, line, lineNo);
+			if (!matchWatch) {
+				// These intentionally do not write to matchWatch as we only want to groups or test runners stored
+				const found = matchName(line, lineNo);
+				if (!found) {
+					testForItem(testReShort, events.onTest, line, lineNo);
+				}
+			}
 		}
 
-        const group = groupRe.exec(line);
-        if (group) {
-			const [match, , , groupName] = group;
-			const range = new vscode.Range(new vscode.Position(lineNo, 0), new vscode.Position(lineNo, match.length));
-			events.onGroup(range, groupName, 1);
-			continue;
+		const matchBracketIndex = matchWatch ? line.lastIndexOf(matchWatch) + matchWatch.length : -1;
+		const openingBrackets: number[] = [];
+		const closingBrackets: number[] = [];
+		for(let charNo = 0; charNo < line.length; charNo++) {
+			const char = line[charNo];
+			if (char == '(') {
+				openingBrackets.push(charNo);
+			} else if (char == ')') {
+				closingBrackets.push(charNo);
+			}	
 		}
 
-        const testRunner = testRunnerRe.exec(line);
-        if (testRunner) {
-			const [match, , , testRunnerName] = testRunner;
-			const range = new vscode.Range(new vscode.Position(lineNo, 0), new vscode.Position(lineNo, match.length));
-			events.onTestRunner(range, testRunnerName);
-			continue;
+		let index = 0;
+		while((openingBrackets.length || closingBrackets.length) && index < line.length) {
+			if (index == openingBrackets[0]) {
+				openingBrackets.shift();
+				bracketIndex++;		
+				if (matchBracketIndex >= index) {
+					bracketWatch.push(bracketIndex);
+				}
+			}
+			if (index == closingBrackets[0]) {
+				if (bracketWatch[bracketWatch.length - 1] == bracketIndex) {
+					bracketWatch.pop();
+					events.onAscend();
+				}
+				closingBrackets.shift();
+				bracketIndex--;
+			}
+
+			index++;
 		}
 	}
 };
